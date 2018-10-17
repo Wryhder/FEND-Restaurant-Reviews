5bc68860119523f12cc816f846384b85590546a4
Wryhder <jemimahsummers@yahoo.com>
2018-10-17 16:32:54 +0100

    Replace text with MapBox API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoid3J5aGRlciIsImEiOiJjam5kYW8wdTYwNXdhM3BubTB4cG82cTJrIn0.8BMlaVrChl-fXIW4hSLGEw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
