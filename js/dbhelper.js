5804faf0d8c20ae84256a9f6e770b79bba271d4a
Wryhder <jemimahsummers@yahoo.com>
2018-10-24 23:38:26 +0100

    Modify database url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,8 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 443 // Change this to your server port
-    //return `http://localhost:${port}/data/restaurants.json`;
-    return `https://github.com:${port}/Wryhder/FEND-Restaurant-Reviews-App/blob/master/data/restaurants.json`;
+    return `/data/restaurants.json`;
+    // return `https://github.com:${port}/Wryhder/FEND-Restaurant-Reviews-App/blob/master/data/restaurants.json`;
   }
 
   /**
