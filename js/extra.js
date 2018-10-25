ce31e9c379932af85effffcec7a000d9bf3c9e86
Wryhder <jemimahsummers@yahoo.com>
2018-10-25 09:17:30 +0100

    Re-add map links to tab order

--- js/extra.js
+++ js/extra.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function (event) {
     // Loop through all anchors on attribution section of map
     // and take them out of tab order
-    const attributionAnchors = document.querySelectorAll('.leaflet-control-attribution a');
-    attributionAnchors.forEach((anchor) => anchor.tabIndex = -1);
+    // const attributionAnchors = document.querySelectorAll('.leaflet-control-attribution a');
+    // attributionAnchors.forEach((anchor) => anchor.tabIndex = -1);
+    // console.log(attributionAnchors);
 });
