95f2b63a430a524541416f47aa3def1d8b60a187
Wryhder <jemimahsummers@yahoo.com>
2018-10-25 09:06:26 +0100

    Remove anchor elements on map attribution section from tab order

--- js/extra.js
+++ js/extra.js
@@ -1,6 +1,6 @@
-// Loop through all anchors on attribution section of map
-// and take them out of tab order
 window.addEventListener('load', function (event) {
+    // Loop through all anchors on attribution section of map
+    // and take them out of tab order
     const attributionAnchors = document.querySelectorAll('.leaflet-control-attribution a');
     attributionAnchors.forEach((anchor) => anchor.tabIndex = -1);
 });
