3d4ac1acc64e3a4e0fb02b43cb166844ff7161c3
Wryhder <jemimahsummers@yahoo.com>
2020-03-13 19:31:31 +0100

    Use relative URL for database data

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,8 +7,9 @@ class DBHelper {
    * Database URL
    */
   static get DATABASE_URL() {
-    const PORT = 5500; // Change this to your server port
-    const RESTAURANT_DATA_URL = `http://localhost:${PORT}/data/restaurants.json`;
+    // const PORT = 5500; // Change this to your server port
+    // const RESTAURANT_DATA_URL = `http://localhost:${PORT}/data/restaurants.json`;
+    const RESTAURANT_DATA_URL = `../data/restaurants.json`;
     return RESTAURANT_DATA_URL;
   }
 
