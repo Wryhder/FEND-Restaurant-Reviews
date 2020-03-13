38b1e6fed427f1fe3e417ba8048ea959cb5691c2
Wryhder <jemimahsummers@yahoo.com>
2020-03-13 19:08:29 +0100

    Change path to service worker
    (Need to test to verify fix) Service worker unable to register properly on hosted site

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Register service worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-  .register('/sw.js')
+  .register('../sw.js')
   .catch(function(err) {
     console.error(err);
   });
